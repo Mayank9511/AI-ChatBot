b27693a2e3d1d446976d31d9e6fbf0e476def57d
Mayank9511 <gupta.mayank.mg01@gmail.com>
2025-03-08 01:16:43 +0530

    adding vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import HeroSection from "./components/HeroSection";
 import ContactUs from "./components/ContactUs";
 import ChatBot from "./components/ChatBot";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   return (
     <>
+      <Analytics />
       <div className="max-w-7xl mx-auto pt-5 px-6">
-        <ChatBot/>
+        <ChatBot />
         <HeroSection />
         <ContactUs />
       </div>
